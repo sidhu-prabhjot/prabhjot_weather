7657849245316386e2914b1af132147a741c71e4
Prabhjot Sidhu <sidhu.prabhjot.2003@gmail.com>
2023-07-19 20:04:37 -0400

    essentially done connecting to API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
-import React, { useState, useEffect, PureComponent } from "react";
+import React, { useState, useEffect } from "react";
 
 /*weather icons*/
-import { WiDaySunny, WiCelsius, WiRaindrop } from "weather-icons-react";
+import {
+  WiDaySunny,
+  WiCelsius,
+  WiRaindrop,
+  WiHumidity,
+  WiStrongWind,
+  WiSmallCraftAdvisory,
+  WiDirectionUp,
+  WiDegrees,
+} from "weather-icons-react";
+
+/*UI icons from feather */
+import { Search } from "react-feather";
 
 /*charts from recharts*/
 import {
@@ -22,75 +34,101 @@ function App() {
 
   //for storing api call data
   const [data, setData] = useState(null);
-  const city = "guelph";
-  const days = 3;
+  const [city, setCity] = useState("");
+  const [region, setRegion] = useState("");
+  const [lastUpdated, setLastUpdated] = useState("");
+  const [condition, setCondition] = useState("");
+  const [temp, setTemp] = useState("");
+  const [feelsLike, setFeelsLike] = useState("");
+  const [hourlyTemp, setHourlyTemp] = useState(null);
+  const [precipitation, setPrecipitation] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [uv, setUV] = useState("");
+  const [pressure, setPressure] = useState("");
+  const [windDirection, setWindDirection] = useState("");
+  const [windSpeed, setWindSpeed] = useState("");
+  const [windDegree, setWindDegree] = useState("");
+
+  //for storing search bar contents
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //function to fetch from API
+  const fetchWeather = (keyCity, days) => {
+    //define weather API endpoint URL
+    const apiURL = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${keyCity}&days=${days}`;
+
+    if (keyCity != null && days != null) {
+      //make API call using fetch
+      fetch(apiURL)
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .then(console.log("fetch"))
+        .catch((error) => console.log("Error fetching data: " + error));
+    }
+  };
 
   useEffect(() => {
-    //define weather API endpoint URL
-    const apiURL = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=${days}`;
-
-    //make API call using fetch
-    // fetch(apiURL)
-    //   .then((response) => response.json())
-    //   .then((data) => setData(data))
-    //   .then(console.log(data))
-    //   .catch((error) => console.log("Error fetching data: " + error));
+    fetchWeather("guelph", 7);
   }, []);
 
-  //data for the hourly weather chart
-  const hourlyData = [
-    {
-      hour: "1",
-      temp: 4,
-    },
-    {
-      hour: "2",
-      temp: 6,
-    },
-    {
-      hour: "3",
-      temp: 8,
-    },
-    {
-      hour: "4",
-      temp: 9,
-    },
-    {
-      hour: "5",
-      temp: 14,
-    },
-    {
-      hour: "6",
-      temp: 18,
-    },
-    {
-      hour: "7",
-      temp: 22,
-    },
-    {
-      hour: "8",
-      temp: 21,
-    },
-    {
-      hour: "9",
-      temp: 15,
-    },
-    {
-      hour: "10",
-      temp: 12,
-    },
-    {
-      hour: "11",
-      temp: 9,
-    },
-    {
-      hour: "12",
-      temp: 4,
-    },
-  ];
+  // Once data is available, set the state variables with destructuring
+  useEffect(() => {
+    if (data) {
+      const {
+        location: { name, region },
+        current: {
+          last_updated,
+          condition: { text },
+          temp_c,
+          feelslike_c,
+          humidity,
+          vis_km,
+          precip_mm,
+          wind_dir,
+          wind_kph,
+          wind_degree,
+          uv,
+          pressure_mb,
+        },
+        forecast: {
+          forecastday: {
+            0: { hour },
+          },
+        },
+      } = data;
+
+      //extracting hourly temp data
+      const tempData = [];
+      for (let i = 0; i < 24; i++) {
+        let hourShift = i + 1;
+        tempData.push({
+          hour: hourShift.toString(),
+          temp: parseFloat(hour[i].temp_c),
+        });
+      }
+
+      setHourlyTemp(tempData);
+
+      setCity(name);
+      setRegion(region);
+      setLastUpdated(last_updated);
+      setCondition(text);
+      setTemp(temp_c);
+      setFeelsLike(feelslike_c);
+      setPrecipitation(precip_mm);
+      setHumidity(humidity);
+      setVisibility(vis_km);
+      setWindDirection(wind_dir);
+      setWindSpeed(wind_kph);
+      setWindDegree(wind_degree);
+      setUV(uv);
+      setPressure(pressure_mb);
+    }
+  }, [data]);
 
   /*percipitation data for radial bar chart*/
-  const percipitationData = [
+  let percipitationData = [
     {
       name: "percipitation",
       amount: 75,
@@ -98,36 +136,71 @@ function App() {
     },
     {
       name: "snow",
-      amount: 25,
+      amount: precipitation,
+      fill: "#82ca9d",
+    },
+  ];
+
+  /*humidity data for radial bar chart*/
+  let humidityData = [
+    {
+      name: "percipitation",
+      amount: 75,
+      fill: "#010",
+    },
+    {
+      name: "snow",
+      amount: humidity,
       fill: "#82ca9d",
     },
   ];
 
   return (
     <div className="App">
-      <div className="menu">Menu</div>
+      <div className="menu">
+        <div className="search-container">
+          <div className="search-bar">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="City name..."
+            />
+          </div>
+          <div
+            className="search-button"
+            onClick={() => fetchWeather(searchTerm, 7)}
+          >
+            <Search size={24} stroke="#fff" />
+          </div>
+        </div>
+        <div className="search-results-container">Results</div>
+        <div className="settings-container">settings</div>
+      </div>
       <div className="data-container">
         <div className="today-forecast-container">
           <div className="inner-container">
             <div className="current-forecast-container">
               <div className="main-title-container">
-                <p className="main-title">Guelph, Ontario</p>
-                <p className="minor-data">last updated: 2023-07-18 22:15</p>
+                <p className="main-title">
+                  {city}, {region}
+                </p>
+                <p className="minor-data">last updated: {lastUpdated}</p>
               </div>
               <div className="weather-container">
                 <div className="actual-weather-container">
                   <WiDaySunny size={24} color="#000"></WiDaySunny>
-                  <p>24</p>
+                  <p>{temp}</p>
                   <WiCelsius size={24} color="#000"></WiCelsius>
                 </div>
                 <div className="feels-like-container">
                   <p>FEELS LIKE</p>
-                  <p>18</p>
+                  <p>{feelsLike}</p>
                   <WiCelsius></WiCelsius>
                 </div>
               </div>
               <div className="weather-type-container">
-                <p>SUNNY</p>
+                <p>{condition}</p>
               </div>
             </div>
             <div className="hourly-forecast-container">
@@ -135,7 +208,7 @@ function App() {
                 <LineChart
                   width={500}
                   height={300}
-                  data={hourlyData}
+                  data={hourlyTemp}
                   margin={{
                     top: 5,
                     right: 30,
@@ -168,7 +241,7 @@ function App() {
                     <p className="title">Percipitation</p>
                   </div>
                   <div className="percipitation-amount-container">
-                    <p className="main-amount-title">9</p>
+                    <p className="main-amount-title">{precipitation}</p>
                     <p>mm</p>
                   </div>
                 </div>
@@ -194,12 +267,12 @@ function App() {
               <div className="inner-container">
                 <div className="data-text-container">
                   <div className="title-container">
-                    <WiRaindrop></WiRaindrop>
+                    <WiHumidity></WiHumidity>
                     <p className="title">Humidity</p>
                   </div>
                   <div className="percipitation-amount-container">
-                    <p className="main-amount-title">9</p>
-                    <p>mm</p>
+                    <p className="main-amount-title">{humidity}</p>
+                    <p>%</p>
                   </div>
                 </div>
                 <ResponsiveContainer className="radial-chart-container">
@@ -207,7 +280,7 @@ function App() {
                     innerRadius="50%"
                     outerRadius="80%"
                     barSize={20}
-                    data={percipitationData}
+                    data={humidityData}
                   >
                     <RadialBar
                       minAngle={15}
@@ -226,37 +299,28 @@ function App() {
               <div className="inner-container">
                 <div className="data-text-container">
                   <div className="title-container">
-                    <WiRaindrop></WiRaindrop>
+                    <WiStrongWind></WiStrongWind>
                     <p className="title">Wind</p>
                   </div>
                   <div className="percipitation-amount-container">
-                    <p className="main-amount-title">9</p>
-                    <p>mm</p>
+                    <p className="main-amount-title">{windSpeed}</p>
+                    <p>kph</p>
                   </div>
                 </div>
-                <ResponsiveContainer className="radial-chart-container">
-                  <RadialBarChart
-                    innerRadius="50%"
-                    outerRadius="80%"
-                    barSize={20}
-                    data={percipitationData}
-                  >
-                    <RadialBar
-                      minAngle={15}
-                      label={{ position: "insideStart", fill: "#010" }}
-                      background
-                      clockWise
-                      dataKey="amount"
-                    />
-                  </RadialBarChart>
-                </ResponsiveContainer>
+                <div className="radial-chart-container">
+                  <WiDirectionUp></WiDirectionUp>
+                  <p>
+                    {windDirection}, {windDegree}
+                    <WiDegrees></WiDegrees>
+                  </p>
+                </div>
               </div>
             </div>
             <div className="other-data-container">
               <div className="inner-container">
                 <div className="data-text-container">
                   <div className="title-container">
-                    <WiRaindrop></WiRaindrop>
+                    <WiSmallCraftAdvisory></WiSmallCraftAdvisory>
                     <p className="title">Other Indexes</p>
                   </div>
                   <div className="percipitation-amount-container">
@@ -264,22 +328,26 @@ function App() {
                     <p>mm</p>
                   </div>
                 </div>
-                <ResponsiveContainer className="radial-chart-container">
-                  <RadialBarChart
-                    innerRadius="50%"
-                    outerRadius="80%"
-                    barSize={20}
-                    data={percipitationData}
-                  >
-                    <RadialBar
-                      minAngle={15}
-                      label={{ position: "insideStart", fill: "#010" }}
-                      background
-                      clockWise
-                      dataKey="amount"
-                    />
-                  </RadialBarChart>
-                </ResponsiveContainer>
+                <div className="radial-chart-container">
+                  <div className="index-container">
+                    <p>UV</p>
+                    <div className="index-indicator-container">
+                      <p>{uv}</p>
+                    </div>
+                  </div>
+                  <div className="index-container">
+                    <p>Visibility</p>
+                    <div className="index-indicator-container">
+                      <p>{visibility} km</p>
+                    </div>
+                  </div>
+                  <div className="index-container">
+                    <p>Pressure</p>
+                    <div className="index-indicator-container">
+                      <p>{pressure} mb</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
